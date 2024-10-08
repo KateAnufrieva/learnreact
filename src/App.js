d4be0252a286300b9483b1e800d16f0a8c5ff375
KateAnufrieva <93079863+KateAnufrieva@users.noreply.github.com>
2024-10-08 20:31:08 +0300

    FormAddUsers

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
 import React from "react"
 import Header from "./components/Header"
 import Users from "./components/Users"
+import AddUser from "./components/AddUser"
 
 class App extends React.Component { 
   
-
-    helpText = "HelpText!"
+    constructor(props){
+        super(props)
+        this.state = {
+            users: [
+                {
+                    id: 1,
+                    firstname: 'Боб',
+                    lastname: 'Иванов',
+                    bio: 'Строитель высшего ранга',
+                    age: 40,
+                    isHappy: true
+                },
+                {
+                    id: 2,
+                    firstname: 'Джон',
+                    lastname: 'Дорл',
+                    bio: 'Инженер',
+                    age: 22,
+                    isHappy: false
+                }
+            ]
+        }
+        this.addUser = this.addUser.bind(this)
+    }
+    
     render() {
         return (<div>
-            <Header title="Список пользователей"/>
+            <Header title="Список пользователей" />
             <main>
-                <Users />
+                <Users users={this.state.users} />
             </main>
-            <aside></aside>
+            <aside>
+                <AddUser onAdd={this.addUser} />
+            </aside>
         </div>)
+    }
+
+    addUser (user) {
+        const id = this.state.users.length + 1 
+        this.setState({ users: [...this.state.users, {id, ...user}] })
     }
 }
 
